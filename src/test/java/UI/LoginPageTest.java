ca400593d2a5dcb43661ee589224ffa9ef9848ff
Aidar Orozakunov <orozakunov2222@gmail.com>
2024-07-11 16:46:55 +0600

    добавление второго теста для LoginPageTest

--- src/test/java/UI/LoginPageTest.java
+++ src/test/java/UI/LoginPageTest.java
@@ -9,7 +9,9 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
@@ -24,29 +26,33 @@ public class LoginPageTest {
     private WebDriverWait wait;
 
     @BeforeClass
-    public void setUp() {
+    public void setUpClass() {
         // Укажите путь к вашему драйверу Chrome
         System.setProperty("webdriver.chrome.driver", "src/main/resources/drivers/chromedriver.exe");
+    }
+
+    @BeforeMethod
+    public void setUpMethod() {
         driver = new ChromeDriver();
         wait = new WebDriverWait(driver, Duration.ofSeconds(20)); // Увеличьте время ожидания
         driver.get("https://testcabinet.salyk.kg/account/login"); // Замените на реальный URL страницы
     }
 
-    @Test
+    @Test(description = "Позитивный тест для авторизации")
     public void testLogin() {
         try {
             // Используем WebDriverWait для ожидания появления элементов
             WebElement loginField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("Username"))); // Найдено по имени поля
             assertNotNull(loginField);
-            System.out.println("ИНН заполнен");
+            log.info("ИНН заполнен");
 
             WebElement passwordField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("Password"))); // Найдено по имени поля
             assertNotNull(passwordField);
-            System.out.println("Пароль прописан");
+            log.info("Пароль прописан");
 
             WebElement loginButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@class='btn btn-primary w-100']"))); // Найдено по тексту кнопки
             assertNotNull(loginButton);
-            System.out.println("Кнопка авторизации успешно нажата");
+            log.info("Кнопка авторизации успешно нажата");
 
             loginField.sendKeys("22405200000946716");
             passwordField.sendKeys("Linux7788.");
@@ -68,8 +74,45 @@ public class LoginPageTest {
         }
     }
 
-    @AfterClass
-    public void tearDown() {
+    @Test(description = "Негативный тест для авторизации")
+    public void testLoginWithInvalidCredentials() {
+        try {
+            WebElement loginInnField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("Username")));
+            assertNotNull(loginInnField);
+            log.info("Обязательное поле ИНН");
+
+            WebElement passwordField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("Password")));
+            assertNotNull(passwordField);
+            log.info("Обязательное поле пароля");
+
+            WebElement clickBtn = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[@class='btn btn-primary w-100']")));
+            assertNotNull(clickBtn);
+            log.info("Кнопка входа в систему");
+
+            loginInnField.sendKeys("22405200000946716");
+            passwordField.sendKeys("Linux7788");
+            clickBtn.click();
+
+            WebElement errorMessage = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[text()='Неправильный логин или пароль.']")));
+            assertNotNull(errorMessage);
+            log.info("Сообщение об ошибке авторизации отображается");
+
+            String expectedErrorText = "Неправильный логин или пароль."; // Убедитесь, что это правильный текст ошибки
+            String actualErrorText = errorMessage.getText();
+            assertTrue(actualErrorText.contains(expectedErrorText), "Неправильный логин или пароль.");
+        } catch (Exception e) {
+            log.error("Вышла ошибка во время теста: " + e.getMessage());
+            throw e;
+        }
+    }
+
+    @AfterMethod
+    public void tearDownMethod() {
         driver.quit();
+    }
+
+    @AfterClass
+    public void tearDownClass() {
+        // Любые действия, которые нужно выполнить после завершения всех тестов
     }
 }
